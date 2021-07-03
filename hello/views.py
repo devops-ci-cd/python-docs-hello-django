59f686e9c6d387590a35c26b28436b2485f92ec9
devops-ci-cd <85459801+devops-ci-cd@users.noreply.github.com>
2021-07-03 23:31:22 +1200

    Update views.py

--- hello/views.py
+++ hello/views.py
@@ -2,4 +2,4 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def hello(request):
-    return HttpResponse("Hello, World!")
+    return HttpResponse("Hello, World! <br> This is task03.2 18 stream homework solution.")
