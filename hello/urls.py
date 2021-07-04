f68b2d7b282d9a6e5c7eb479ea69ed67b0dddbec
devops-ci-cd <85459801+devops-ci-cd@users.noreply.github.com>
2021-07-04 13:43:09 +1200

    Update urls.py

--- hello/urls.py
+++ hello/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [    
+    path('', views.hello, name='hello'),
+    # for index.html health probe purpose
     path('index.html/', views.hello, name='hello'),
 ]
