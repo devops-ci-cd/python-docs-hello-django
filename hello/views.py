5a2d007c45d2a84dcf26a3a764f58143f054006c
devops-ci-cd <85459801+devops-ci-cd@users.noreply.github.com>
2021-08-05 11:00:22 +1200

    Update views.py

--- hello/views.py
+++ hello/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+import os
 
 def hello(request):
-    return HttpResponse("Hello, World! <br> CloudX solution <br> from east-us location")
+    return HttpResponse("Hello, World! <br> CloudX solution <br> from east-us location <br> Connections string: <br>" + str(os.environ['ConVar']))
